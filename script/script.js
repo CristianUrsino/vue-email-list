50496dc93a3f7d7c2785d7604941831134176bfe
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-07 15:39:18 +0100

    initial commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        emailList: [],
+      }
+    },
+    methods: {
+    },
+    mounted(){
+        
+    }
+  }).mount('#app')
